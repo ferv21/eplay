81444e44c059d254cfddc6424c0bc7bbf44d311d
Fernando Viana <fernandovianadossantos2001@gmail.com>
2023-05-30 17:11:33 -0300

    criando carrinho de compras

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { store } from './store'
 import Header from './components/Header'
 import GlobalCss from './styles'
 import Footer from './components/Footer'
+import Cart from './components/Cart'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           </div>
           <Rotas />
           <Footer />
+          <Cart />
         </BrowserRouter>
       </Provider>
     </>
